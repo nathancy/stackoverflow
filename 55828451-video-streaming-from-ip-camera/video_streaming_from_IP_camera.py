cb3a1db3c10d54848c2a0f7fc7c7d50da1fcef36
Nathan Lam <nathancy@hawaii.edu>
2019-04-24 11:40:28 -1000

    Video streaming from IP camera

---
+++ 55828451-video-streaming-from-ip-camera/video_streaming_from_IP_camera.py
@@ -0,0 +1,64 @@
+from threading import Thread
+import cv2
+
+class VideoStreamWidget(object):
+    def __init__(self, src=0):
+        # Create a VideoCapture object
+        self.capture = cv2.VideoCapture(src)
+
+        # Start the thread to read frames from the video stream
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+
+    def show_frame(self):
+        # Display frames in main program
+        if self.status:
+            self.frame = self.maintain_aspect_ratio_resize(self.frame, width=600)
+            cv2.imshow('IP Camera Video Streaming', self.frame)
+
+        # Press Q on keyboard to stop recording
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            cv2.destroyAllWindows()
+            exit(1)
+
+    # Resizes a image and maintains aspect ratio
+    def maintain_aspect_ratio_resize(self, image, width=None, height=None, inter=cv2.INTER_AREA):
+        # Grab the image size and initialize dimensions
+        dim = None
+        (h, w) = image.shape[:2]
+
+        # Return original image if no need to resize
+        if width is None and height is None:
+            return image
+
+        # We are resizing height if width is none
+        if width is None:
+            # Calculate the ratio of the height and construct the dimensions
+            r = height / float(h)
+            dim = (int(w * r), height)
+        # We are resizing width if height is none
+        else:
+            # Calculate the ratio of the 0idth and construct the dimensions
+            r = width / float(w)
+            dim = (width, int(h * r))
+
+        # Return the resized image
+        return cv2.resize(image, dim, interpolation=inter)
+
+if __name__ == '__main__':
+    stream_link = 'your stream link!'
+    video_stream_widget = VideoStreamWidget(stream_link)
+    while True:
+        try:
+            video_stream_widget.show_frame()
+        except AttributeError:
+            pass
