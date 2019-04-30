b1974a5b60868b4a4f4fecf62038625d8c1977dd
Nathan Lam <nathancy@hawaii.edu>
2019-04-29 17:01:47 -1000

    four point perspective transform

---
+++ 55908128-perspective-transform/perspective-transform.py
@@ -0,0 +1,78 @@
+import imutils
+import cv2
+import numpy as np
+
+def order_corner_points(corners):
+    # Separate corners into individual points
+    # Index 0 - top-right
+    #       1 - top-left
+    #       2 - bottom-left
+    #       3 - bottom-right
+    corners = [(corner[0][0], corner[0][1]) for corner in corners]
+    top_r, top_l, bottom_l, bottom_r = corners[0], corners[1], corners[2], corners[3]
+    return (top_l, top_r, bottom_r, bottom_l)
+
+def perspective_transform(image, corners):
+
+    # Order points in clockwise order
+    ordered_corners = order_corner_points(corners)
+    top_l, top_r, bottom_r, bottom_l = ordered_corners
+    
+    # Determine width of new image which is the max distance between 
+    # (bottom right and bottom left) or (top right and top left) x-coordinates
+    width_A = np.sqrt(((bottom_r[0] - bottom_l[0]) ** 2) + ((bottom_r[1] - bottom_l[1]) ** 2))
+    width_B = np.sqrt(((top_r[0] - top_l[0]) ** 2) + ((top_r[1] - top_l[1]) ** 2))
+    width = max(int(width_A), int(width_B))
+
+    # Determine height of new image which is the max distance between 
+    # (top right and bottom right) or (top left and bottom left) y-coordinates
+    height_A = np.sqrt(((top_r[0] - bottom_r[0]) ** 2) + ((top_r[1] - bottom_r[1]) ** 2))
+    height_B = np.sqrt(((top_l[0] - bottom_l[0]) ** 2) + ((top_l[1] - bottom_l[1]) ** 2))
+    height = max(int(height_A), int(height_B))
+
+    # Construct new points to obtain top-down view of image in 
+    # top_r, top_l, bottom_l, bottom_r order
+    dimensions = np.array([[0, 0], [width - 1, 0], [width - 1, height - 1], 
+                    [0, height - 1]], dtype = "float32")
+    
+    # Convert to Numpy format
+    ordered_corners = np.array(ordered_corners, dtype="float32")
+
+    # Find perspective transform matrix
+    matrix = cv2.getPerspectiveTransform(ordered_corners, dimensions)
+
+    # Return the transformed image
+    return cv2.warpPerspective(image, matrix, (width, height))
+
+image = cv2.imread('1.jpg')
+ratio = image.shape[0] / 300.0
+image = imutils.resize(image, height=300)
+realImage = image.copy()
+
+# convert the image to grayscale, blur it, and find edges in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+edged = cv2.Canny(gray, 30, 200)
+
+cnts = cv2.findContours(edged.copy(), cv2.RETR_TREE, 
+cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+screenCnt = None
+
+# loop over our contours
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.015 * peri, True)
+
+    if len(approx) == 4:
+        screenCnt = approx
+        transformed = perspective_transform(realImage, screenCnt)
+        break
+
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 3)
+cv2.imshow("image", realImage)
+cv2.imshow("Screen Rec", image)
+cv2.imshow("transformed", transformed)
+cv2.waitKey(0)
