6f7200f74a0c9a6a41f59b2a383da7d7d66a3e16
Nathan Lam <nathancy@hawaii.edu>
2019-08-06 16:17:42 -1000

    Mask background white and merge contour into single rectangle

--- 57382935-mask-background-white/mask_background_white.py
+++ 57382935-mask-background-white/mask_background_white.py
@@ -9,16 +9,30 @@ upper = np.array([145, 255, 255])
 mask = cv2.inRange(image, lower, upper)
 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-close = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
+opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+close = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=2)
 
 cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-result[close==0] = (255,255,255)
+boxes = []
+for c in cnts:
+    (x, y, w, h) = cv2.boundingRect(c)
+    boxes.append([x,y, x+w,y+h])
 
-dilate = cv2.dilate(close, kernel, iterations=2)
+boxes = np.asarray(boxes)
+left = np.min(boxes[:,0])
+top = np.min(boxes[:,1])
+right = np.max(boxes[:,2])
+bottom = np.max(boxes[:,3])
+
+result[close==0] = (255,255,255)
+ROI = result[top:bottom, left:right].copy()
+cv2.rectangle(result, (left,top), (right,bottom), (36, 255, 12), 2)
 
 cv2.imshow('result', result)
-cv2.imshow('dilate', dilate)
+cv2.imshow('ROI', ROI)
 cv2.imshow('close', close)
+cv2.imwrite('result.png', result)
+cv2.imwrite('ROI.png', ROI)
 cv2.waitKey()
