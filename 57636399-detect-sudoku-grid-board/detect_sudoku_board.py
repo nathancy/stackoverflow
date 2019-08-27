3256eb8d8a91db632c35c11e65f59aaa73bc5278
Nathan Lam <nathancy@hawaii.edu>
2019-08-27 10:46:59 -1000

    Update

--- 57636399-detect-sudoku-grid-board/detect_sudoku_board.py
+++ 57636399-detect-sudoku-grid-board/detect_sudoku_board.py
@@ -45,10 +45,10 @@ def perspective_transform(image, corners):
 image = cv2.imread('1.jpg')
 original = image.copy()
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-opening = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
+blur = cv2.medianBlur(gray, 3)
+thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,3)
 
-cnts = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)
 
@@ -58,7 +58,6 @@ for c in cnts:
     transformed = perspective_transform(original, approx)
     break
 
-cv2.imshow('original', original)
 cv2.imshow('transformed', transformed)
 cv2.imwrite('board.png', transformed)
 cv2.waitKey()
