d18313606072d590eafec29bef102e14a5f86f00
Nathan Lam <nathancy@hawaii.edu>
2019-08-30 11:30:44 -1000

    Update

--- 57260893-improve-horizontal-line-detection/improve_horizontal_line_detection.py
+++ 57260893-improve-horizontal-line-detection/improve_horizontal_line_detection.py
@@ -5,15 +5,15 @@ gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
 horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (15,1))
-remove_vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
 
-cnts = cv2.findContours(remove_vertical, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
 for c in cnts:
     cv2.drawContours(image, [c], -1, (36,255,12), 3)
 
 cv2.imshow('thresh', thresh)
-cv2.imshow('remove_vertical', remove_vertical)
+cv2.imshow('detected_lines', detected_lines)
 cv2.imshow('image', image)
 cv2.waitKey()
