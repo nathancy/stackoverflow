462e80e422b8a9cc8e938dbf58a275c96bd54a42
Nathan Lam <nathancy@hawaii.edu>
2019-09-09 11:02:15 -1000

    Removing slant horizontal lines

---
+++ 57834671-removing-slant-horizontal-lines/removing_slant_horizontal_lines.py
@@ -0,0 +1,19 @@
+import cv2
+
+image = cv2.imread('1.jpg')
+gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (45,1))
+detected_lines = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
+
+cnts = cv2.findContours(detected_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+for c in cnts:
+    cv2.drawContours(image, [c], -1, (255,255,255), 3)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('detected_lines', detected_lines)
+cv2.imshow('image', image)
+cv2.waitKey()
