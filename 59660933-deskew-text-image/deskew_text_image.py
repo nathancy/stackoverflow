78635d6cf24203e1e3886449a2e23a901f298d51
Nathan Lam <nathancy@hawaii.edu>
2020-01-09 12:01:23 -1000

    Deskew text image

---
+++ 59660933-deskew-text-image/deskew_text_image.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+from scipy.ndimage import interpolation as inter
+
+def correct_skew(image, delta=.25, limit=5):
+    def determine_score(arr, angle):
+        data = inter.rotate(arr, angle, reshape=False, order=0)
+        histogram = np.sum(data, axis=1)
+        score = np.sum((histogram[1:] - histogram[:-1]) ** 2)
+        return histogram, score
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.medianBlur(gray, 3)
+    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1] 
+
+    scores = []
+    angles = np.arange(-limit, limit + delta, delta)
+    for angle in angles:
+        histogram, score = determine_score(thresh, angle)
+        scores.append(score)
+
+    best_angle = angles[scores.index(max(scores))]
+
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, best_angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, \
+              borderMode=cv2.BORDER_REPLICATE)
+
+    return best_angle, rotated
+
+if __name__ == '__main__':
+    image = cv2.imread('1.jpg')
+    angle, rotated = correct_skew(image)
+    print(angle)
+    cv2.imshow('rotated', rotated)
+    cv2.imwrite('rotated.png', rotated)
+    cv2.waitKey()
